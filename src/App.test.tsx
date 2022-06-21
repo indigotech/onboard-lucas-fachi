116ed806edbac7d5dc435d5860ee83fd770df99c
lucasfac <lucasstefenonfachini@gmail.com>
2022-06-21 20:47:34 -0300

    update setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { debug } from 'console';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  debugger;
-  ReactDOM.render(<App />, div);
 });
